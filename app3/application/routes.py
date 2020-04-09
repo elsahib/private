db9fc29006dd52c78a4d55b3010107d3836792e5
Osman Elsahib <elsahib20@gmail.com>
2020-04-09 14:55:21 +0100

    hi

--- app3/application/routes.py
+++ app3/application/routes.py
@@ -1,1 +1,10 @@
 from application import app
+from flask import request, Response
+import string, random
+
+
+@app.route('/num', methods =['GET'] )
+def num():
+    result = ''.join(random.choices(string.digits, k = 6))
+
+    return Response(result, mimetype='text/plain')
