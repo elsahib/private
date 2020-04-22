3499f216907381accf50a9354dc9d1597cdd512e
Osman Elsahib <elsahib20@gmail.com>
2020-04-22 12:17:39 +0100

    All works, but need media queries for display

--- app1/chkDB.sh
+++ app1/chkDB.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-sleep 1m
+sleep 5
 DBTABLES="`mysql -h mysql -u${DBUser} -p${MYSQL_ROOT_PASSWORD} --batch -N -e "use prizes;show table status;" | gawk 'BEGIN {ORS=", " } $2 == "MyISAM" || $2 == "InnoDB"{print "\`" $1 "\`"}' | sed 's/, $//'`"
 if [ -z "${DBTABLES// }" ]
 then
