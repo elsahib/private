a88689c40d514736a80ef96e37b99dac3925b60c
elsahib10@gmail.com <elsahib10@gmail.com>
2020-04-24 16:50:06 +0100

    Dockerfile of app1 Modified

--- app1/chkDB.sh
+++ app1/chkDB.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 sleep 5
-DBTABLES="`mysql -h mysql -u${DBUser} -p${MYSQL_ROOT_PASSWORD} --batch -N -e "use prizes;show table status;" | gawk 'BEGIN {ORS=", " } $2 == "MyISAM" || $2 == "InnoDB"{print "\`" $1 "\`"}' | sed 's/, $//'`"
+DBTABLES="`mysql -h${DBserv} -u${DBUser} -p${MYSQL_ROOT_PASSWORD} --batch -N -e "use prizes;show table status;" | gawk 'BEGIN {ORS=", " } $2 == "MyISAM" || $2 == "InnoDB"{print "\`" $1 "\`"}' | sed 's/, $//'`"
 if [ -z "${DBTABLES// }" ]
 then
 exec python create.py
